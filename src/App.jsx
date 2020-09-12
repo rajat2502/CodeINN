cf79ad14546f8e6fde973552dc4c293ae2354d1f
Rajat Verma <rajatverma5885045@gmail.com>
2020-09-12 10:07:36 +0530

    feat(ui): add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from 'components/Navbar';
 
 function App() {
+  const [user, setUser] = useState({});
+
   return (
     <div className='App'>
-      <h1>CodeINN</h1>
+      <Router>
+        <Navbar user={user} setUser={setUser} />
+        <Switch></Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+const Navbar = ({ user, setUser }) => {
+  const history = useHistory();
+
+  const logOut = () => {
+    setUser({});
+    localStorage.clear();
+    history.push('/login');
+  };
+
+  return (
+    <div
+      className='bg-gray-900 flex justify-between items-center py-4 px-2'
+      style={{ height: 70 }}
+    >
+      <Link
+        to='/'
+        className='mx-2 flex items-center text-2xl text-white font-bold'
+      >
+        <img
+          src={require('assets/img/logo.png')}
+          className='h-12'
+          alt='CodeINN'
+        />
+        <span className='font-normal text-lg'>
+          &nbsp; - &nbsp;an online code editor
+        </span>
+      </Link>
+      {!user.username ? (
+        <Link
+          to='/login'
+          className='mx-2 px-6 py-1 text-white font-bold text-lg border border-white rounded'
+        >
+          Login
+        </Link>
+      ) : (
+        <div className='p-10'>
+          <div className='dropdown inline-block relative'>
+            <button className='text-white text-lg font-bold py-2 px-4 inline-flex items-center'>
+              <span className='mr-1'>
+                {JSON.parse(localStorage.getItem('user')) &&
+                  JSON.parse(localStorage.getItem('user')).username}
+              </span>
+            </button>
+            <ul className='z-10 dropdown-menu absolute hidden text-black pt-1'>
+              <li className=''>
+                <Link
+                  to='/dashboard'
+                  className='rounded-t bg-gray-400 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap'
+                >
+                  Dashboard
+                </Link>
+              </li>
+              <li className=''>
+                <Link
+                  to='/web/new'
+                  className='bg-gray-400 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap'
+                >
+                  New Web Pen
+                </Link>
+              </li>
+              <li className=''>
+                <Link
+                  to='/programming/new'
+                  className='bg-gray-400 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap'
+                >
+                  New Program
+                </Link>
+              </li>
+              <li
+                className='cursor-pointer rounded-b bg-gray-400 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap'
+                onClick={logOut}
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
